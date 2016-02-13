9a832ec76b2167f8c68640bc9214a7219734fde0
Sinan Akpolat <aciliketcap@gmail.com>
2016-02-14 01:36:15 +0200

    Improve MAC - IP hash
    Use hash function
    New function to create entry with new mac
    Prettify debug messages
    Update comments
    Free skb if we can't create new MAC address

--- fakeARP.c
+++ fakeARP.c
@@ -21,10 +21,8 @@
  * This code is meant as a tutorial and does some stuff wrong, also doesn't do
  * some stuff it should be doing. I didn't introduce some features to keep it simple.
  * 1) It would be cooler if it used random mac addresses.
- * And keep track of them, you know like a private arp table.
- * We're lying and we should be consistent.
- * Currently it just uses cc:cc:cc:cc:cc:cc as a MAC addr everytime
- * 2) A minor thing is normally device stats are increased in a per-CPU manner.
+ * 2) add / remove IP - MAC pairs and show them from sysfs (debug with procfs first)
+ * 3) write scripts to test these easily and share them
  */
 
 #include <linux/module.h> //for init/exit macros
@@ -76,11 +74,18 @@ struct ip_mac_pair {
 
 //TODO: implement locking for this global (for now incoming lock is enough)
 //TODO: I don't need to zero these out, they are already in BSS, right?
+//very simple hash table implementation
 static struct list_head fake_mac_list[256];
+
+//hash function
+struct list_head *hash_fake_mac_list(u8 *ip) {
+	//hash it by the last byte of the IP address
+	//no collision for LAN addresses like 192.168.1.x
+	return &fake_mac_list[*(ip+3)];
+}
 
 //this will be used in bottom half tasklet
 u8 *insert_ip_mac_pair(u8 *ip, u8 *mac) {
-
 	struct ip_mac_pair *new_pair = kmalloc(sizeof(struct ip_mac_pair), GFP_KERNEL);
 	if(!new_pair)
 		return 0;
@@ -88,31 +93,38 @@ u8 *insert_ip_mac_pair(u8 *ip, u8 *mac) {
 	memcpy(new_pair->ip, ip, 4);
 	memcpy(new_pair->mac, mac, 6);
 
-//	if(!fake_mac_list[*ip]) {
-//		fake_mac_list[*ip] = &new_pair->mac_list;
-//	} else { //collision
-		printk(KERN_DEBUG "collision\n");
-		list_add(&new_pair->mac_list, &fake_mac_list[*ip]);
-//	}
+	list_add(&new_pair->mac_list, hash_fake_mac_list(ip));
 
 	return new_pair->mac;
 }
 
-//TODO: we don't need a remove_ip_hash function for now
+u8 *insert_new_ip_mac_pair(u8 *ip) {
+	u64 n_next_mac = cpu_to_be64(next_mac);
+	u8 *next_mac_ptr = ((u8*) &n_next_mac) + 2;
+	u8 *ret;
 
-//return pointer to mac address
+	ret = insert_ip_mac_pair(ip, next_mac_ptr);
+	next_mac++;
+
+	return ret;
+}
+
+//TODO: we don't need a remove_ip_hash function for now, but it will be written eventually
+
+//return pointer to mac address which is associated with ip
 u8 *get_mac(u8 *ip) {
 	struct ip_mac_pair *tmp;
 
-	if(list_empty(&fake_mac_list[*ip])) {
-		printk(KERN_DEBUG "interestingly list pointer is empty?\n");
+	if(list_empty(hash_fake_mac_list(ip))) {
+		printk(KERN_DEBUG "No MAC recorded for IP %pI4 before\n", ip);
 		return 0;
 	} else {
 		printk(KERN_DEBUG "loop da list %c\n", *ip);
 		//compare full mac, loop until we find it or list is over
-		list_for_each_entry(tmp, &fake_mac_list[*ip], mac_list) {
-			printk(KERN_DEBUG "looking at list ip %c%c%c%c and new ip %c%c%c%c\n", *tmp->ip, *tmp->ip+1,*tmp->ip+2,*tmp->ip+3,*ip,*ip+1,*ip+2,*ip+3);
-			if(memcmp(tmp->ip, ip, 4)) { //proceed to next one
+		list_for_each_entry(tmp, hash_fake_mac_list(ip), mac_list) {
+			printk(KERN_DEBUG "looking at list ip %pI4 and new ip %pI4\n", tmp->ip, ip);
+
+			if(memcmp(tmp->ip, ip, 4)) { //proceed to next one if cmp fails
 				continue;
 			} else {
 				return tmp->mac;
@@ -120,7 +132,27 @@ u8 *get_mac(u8 *ip) {
 		}
 	}
 
+	printk(KERN_DEBUG "No MAC recorded for IP %pI4 before\n", ip);
 	return 0;
+}
+
+//for better debugging
+void dump_ip_list(void) {
+	struct ip_mac_pair *tmp;
+	int i;
+
+	for(i=0;i<256;i++) {
+		if(list_empty(&fake_mac_list[i]))
+			printk(KERN_NOTICE "hash %d is empty\n", i);
+		else {
+			printk(KERN_NOTICE "IPs in hash %d\n", i);
+			list_for_each_entry(tmp, &fake_mac_list[i], mac_list) {
+				printk(KERN_NOTICE "IP %pI4 has mac %pM\n", tmp->ip, tmp->mac);
+			}
+		}
+	}
+
+	return;
 }
 
 //this just prevents cache invalidation on all cpus when one cpu updates stats
@@ -330,18 +362,17 @@ void fakeARP(unsigned long noparam) {
 			memcpy(data, orgdata+ethersrc, 6); //copy src mac to dest mac field
 
 			if((my_mac = get_mac(data+targetIP)) == 0) {
-				u32 dbg_ip;
-				memcpy(&dbg_ip, data+targetIP, 4);
-				printk(KERN_DEBUG "unable to find IP %x in IP list\n", dbg_ip); //TODO: print IP properly
+				printk(KERN_DEBUG "unable to find IP %pI4 in IP list\n", data+targetIP);
+				if((my_mac = insert_new_ip_mac_pair(data+targetIP)) == 0) {
+					printk(KERN_CRIT "unable to create new MAC for IP %pI4\n", data+targetIP);
+					//leave incoming in the list, may be we can do it next time. Remove failed one.
+					kfree_skb(fake_skb);
 
-				u8 *next_mac_ptr = &next_mac;
-				printk(KERN_DEBUG "trying to add %x IP as MAC %llu\n", dbg_ip, next_mac);
-				if((my_mac = insert_ip_mac_pair(data+targetIP, next_mac_ptr)) == 0) {
-					//TODO: free skb, it is officially dropped now
-					printk(KERN_CRIT "unable to create new MAC for IP"); //TODO: print IP properly
 					return;
 				}
-				next_mac++;
+
+				dump_ip_list();
+
 			}
 
 			memset(data+ethersrc, 0xcc, 6); //for now all IP owners claim their mac is cc:cc:cc:cc:cc:cc
